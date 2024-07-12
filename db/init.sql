8702a78f2fd8bc1349e6ee88bb66f6fdf5f6c1a6
Tomas Miola <tomasmiola71@gmail.com>
2024-07-12 17:05:31 -0300

    modified database example schema

--- db/init.sql
+++ db/init.sql
@@ -1,12 +1,15 @@
-CREATE DATABASE IF NOT EXISTS products;
-
-CREATE TABLE Flavors(
-    id_sabor INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(100) NOT NULL
-);
+CREATE DATABASE IF NOT EXISTS contacts;
 
 CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (email)
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
+    phone VARCHAR(20),
+    user_id INT NOT NULL
 );
